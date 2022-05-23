4291e6e50d4d6792fdf9c568cfedb54f4a3c26d2
benjamin <benjamin.morel@students.hevs.ch>
2022-05-23 11:15:18 +0200

    Register view

--- EFCoreApp2021/Program.cs
+++ EFCoreApp2021/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using Agricathon2022;
 
 namespace EFCoreApp2021
 {
@@ -8,8 +9,6 @@ namespace EFCoreApp2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             var ctx = new AgricathonContext();
 
             var e = ctx.Database.EnsureCreated();
@@ -19,7 +18,27 @@ namespace EFCoreApp2021
             else
                 Console.WriteLine("Database already exists");
 
-            Console.WriteLine("done."); 
+            Console.WriteLine("done.");
+
+            populateDatabase(ctx);
+
         }
+
+        private static void populateDatabase(AgricathonContext ctx)
+        {
+            Exploitant exploitant1 = new Exploitant() { NoExploitant="742", Firstname="Doriane", Lastname="Papilloud", Address="Chemin de Sierre 12", Email="doriane@gmail.ch", Password="password"};
+            ctx.ExploitantSet.Add(exploitant1);
+
+            Proprietaire proprietaire1 = new Proprietaire() { Firstname = "Gaétan", Lastname = "Mottet", Address="Route des vignobles 3", Email="gaetan@gmail.ch", Password="password" };
+            ctx.ProprietaireSet.Add(proprietaire1);
+
+
+
+
+            ctx.SaveChanges();
+
+        }
+
+
     }
 }
