82d228bafbd26d668f3747ba2aca55f136c324f1
megane <megane.solliard@students.hevs.ch>
2022-05-23 09:59:52 +0200

    Services in startup add

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -1,6 +1,8 @@
+using EFCoreApp2021;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,8 @@ namespace WebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AgricathonContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
